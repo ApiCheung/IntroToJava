e1bd67fe826baf0d5fd0bcb8eb960df8da4a2d29
EsmeeZhang <esmezh42@gmail.com>
2020-09-18 22:10:41 -0400

    week1

--- Assignment1/PartI/PrintResults.java
+++ Assignment1/PartI/PrintResults.java
@@ -4,7 +4,7 @@ public class PrintResults {
 
 	public static void main(String[] args) {
 		
-		System.out.println("The value of 1+1 is " + (1+1));
+		//System.out.println("The value of 1+1 is " + (1+1));
 		int a = 10 + 5;
 		int b = 50 - 23;
 		int c = 12 * 13;
